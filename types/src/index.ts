6180f8fdcdf54e8f1981331cafcf98d464702feb
delProfundo <bruno@hypermedia.au>
2025-06-28 16:49:30 +1000

    added stateful set test chart

--- types/src/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Auto-generated barrel export for workload schemas
- * DO NOT EDIT - This file is auto-generated
- */
-
-export * from './basic-container-load';
-export * from './workload-types';
